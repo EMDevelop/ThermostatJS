a11bdba2ef6148cca96ba175d094db7a81cc6009
EMDevelop <edwardmartin.inbox@gmail.com>
2021-09-29 20:40:14 +0100

    added a max

--- src/thermostat.js
+++ src/thermostat.js
@@ -2,12 +2,15 @@ class Thermostat {
   constructor() {
     this.temperature = 20;
     this.MIN_TEMPERATURE = 10;
+    this.MAX_TEMPERATURE = 25;
   }
   getTemp() {
     return this.temperature;
   }
   up(change) {
-    this.temperature += change;
+    this._isMaxTemp(this.temperature - change)
+      ? (this.temperature = this.MAX_TEMPERATURE)
+      : (this.temperature += change);
   }
   down(change) {
     this._isMinTemp(this.temperature - change)
@@ -15,8 +18,11 @@ class Thermostat {
       : (this.temperature -= change);
   }
 
-  _isMinTemp(input) {
-    // debugger;
-    return input < this.MIN_TEMPERATURE;
+  _isMinTemp(change) {
+    return change < this.MIN_TEMPERATURE;
+  }
+
+  _isMaxTemp(change) {
+    return change < this.MAX_TEMPERATURE;
   }
 }
