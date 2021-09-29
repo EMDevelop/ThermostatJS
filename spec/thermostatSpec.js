eeddc1f508c9d0cc5a57a2ec2fa71c7e7ae16720
EMDevelop <edwardmartin.inbox@gmail.com>
2021-09-29 19:50:55 +0100

    ctemp cannot go below 10

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -3,14 +3,23 @@ describe("Thermostat", function () {
     thermostat = new Thermostat();
   });
   it("Starts at 20", function () {
+    console.log("test 1");
     expect(thermostat.getTemp()).toBe(20);
   });
   it("Temperature can be increased", function () {
+    console.log("test 2");
     thermostat.up(10);
     expect(thermostat.getTemp()).toBe(30);
   });
   it("Temperature can be decreased", function () {
+    console.log("test 3");
     thermostat.down(5);
     expect(thermostat.getTemp()).toBe(15);
+  });
+
+  it("Temperature cannot go below 10", function () {
+    console.log(thermostat.getTemp());
+    thermostat.down(10);
+    expect(thermostat.getTemp()).toBe(10);
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,6 +1,7 @@
 class Thermostat {
   constructor() {
     this.temperature = 20;
+    this.MIN_TEMPERATURE = 10;
   }
   getTemp() {
     return this.temperature;
@@ -9,6 +10,13 @@ class Thermostat {
     this.temperature += change;
   }
   down(change) {
-    this.temperature -= change;
+    this._isMinTemp(this.temperature - change)
+      ? (this.temperature = this.MIN_TEMPERATURE)
+      : (this.temperature -= change);
+  }
+
+  _isMinTemp(input) {
+    // debugger;
+    return input < this.MIN_TEMPERATURE;
   }
 }
